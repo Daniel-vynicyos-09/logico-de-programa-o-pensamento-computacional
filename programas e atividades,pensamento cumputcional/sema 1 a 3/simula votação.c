73e1fc803ad7a3d894ccbbaf0fa68d4a121c3b21
Daniel-vynicyos-09 <danielvynicyos07@gmail.com>
2026-07-30 11:55:50 -0300

    Update simula votação.c

--- programas e atividades,pensamento cumputcional/sema 1 a 3/simula votação.c
+++ programas e atividades,pensamento cumputcional/sema 1 a 3/simula votação.c
@@ -2,119 +2,86 @@
 #include <string.h>
 
 int main() {
-    // Cadastro dos candidatos
-    char nome1[50];
-    char nome2[50];
 
-    int codigo1;
-    int codigo2;
+    char nome1[50], nome2[50], aux[50];
+    int codigo1, codigo2, auxCodigo;
+
+    int voto;
+    int votos1 = 0, votos2 = 0;
+    int branco = 0, nulo = 0;
+    int total = 0;
 
     printf("Digite o nome do primeiro candidato: ");
     fgets(nome1, 50, stdin);
-
-    // Remove o ENTER (\n) que o fgets guarda
     nome1[strcspn(nome1, "\n")] = '\0';
 
-    printf("Digite o número de %s: ", nome1);
+    printf("Digite o codigo do primeiro candidato: ");
     scanf("%d", &codigo1);
-
-    // Limpa o ENTER que ficou no teclado
     getchar();
 
-    printf("\nDigite o nome do segundo candidato: ");
+    printf("Digite o nome do segundo candidato: ");
     fgets(nome2, 50, stdin);
-
-    // Remove o ENTER
     nome2[strcspn(nome2, "\n")] = '\0';
 
-    printf("Digite o número de %s: ", nome2);
+    printf("Digite o codigo do segundo candidato: ");
     scanf("%d", &codigo2);
 
+    if (strcmp(nome1, nome2) > 0) {
+        strcpy(aux, nome1);
+        strcpy(nome1, nome2);
+        strcpy(nome2, aux);
 
-    // Contadores de votos
-    int votos1 = 0;
-    int votos2 = 0;
-    int brancos = 0;
-    int nulos = 0;
+        auxCodigo = codigo1;
+        codigo1 = codigo2;
+        codigo2 = auxCodigo;
+    }
 
-    int voto;
+    do {
 
-
-    // Votação
-    while (1) {
-
-        printf("\n------------------------------\n");
-        printf("MENU:\n");
-
+        printf("\n-----------------------------\n");
+        printf("Menu:\n");
         printf("Voto em %s: %d\n", nome1, codigo1);
         printf("Voto em %s: %d\n", nome2, codigo2);
-
         printf("Voto em Branco: 0\n");
-        printf("Encerrar votação: -1 ou qualquer valor negativo\n");
-
-        printf("\nDigite sua opção: ");
+        printf("Encerrar votacao: -1\n");
+        printf("Digite sua opcao: ");
         scanf("%d", &voto);
 
+        if (voto >= 0) {
 
-        // Encerra a votação
-        if (voto < 0) {
-            break;
+            total++;
+
+            if (voto == codigo1) {
+                votos1++;
+            }
+             else if (voto == codigo2) {
+                votos2++;
+            }
+            else if (voto == 0) {
+                branco++;
+            }
+            else {
+                nulo++;
+            }
+
         }
 
-        // Voto em branco
-        else if (voto == 0) {
-            brancos++;
-        }
+    } while (voto >= 0);
 
-        // Voto no candidato 1
-        else if (voto == codigo1) {
-            votos1++;
-        }
-
-        // Voto no candidato 2
-        else if (voto == codigo2) {
-            votos2++;
-        }
-
-        // Qualquer outro número é voto nulo
-        else {
-            nulos++;
-        }
-    }
-
-
-    // Total de votos
-    int total = votos1 + votos2 + brancos + nulos;
-
-
-    // Percentuais
-    float perc_brancos;
-    float perc_nulos;
+    printf("\n========== RESULTADO ==========\n");
+    printf("Total de votos: %d\n", total);
+    printf("%s: %d votos\n", nome1, votos1);
+    printf("%s: %d votos\n", nome2, votos2);
+    printf("Votos em branco: %d\n", branco);
+    printf("Votos nulos: %d\n", nulo);
 
     if (total > 0) {
-        perc_brancos = ((float) brancos / total) * 100;
-        perc_nulos = ((float) nulos / total) * 100;
+        printf("Percentual de votos em branco: %.2f%%\n", (branco * 100.0) / total);
+        printf("Percentual de votos nulos: %.2f%%\n", (nulo * 100.0) / total);
     }
     else {
-        perc_brancos = 0;
-        perc_nulos = 0;
+        printf("Nenhum voto foi registrado.\n");
     }
-
-
-    // Resultado
-    printf("\n========== RESULTADO ==========\n");
-
-    printf("Total geral de votos: %d\n", total);
-
-    printf("Votos para %s: %d\n", nome1, votos1);
-    printf("Votos para %s: %d\n", nome2, votos2);
-
-    printf("Votos em branco: %d\n", brancos);
-    printf("Votos nulos: %d\n", nulos);
-
-    printf("Percentual de votos em branco: %.2f%%\n", perc_brancos);
-    printf("Percentual de votos nulos: %.2f%%\n", perc_nulos);
-
 
     return 0;
 }
